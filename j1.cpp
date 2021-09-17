8296a68067e177ab8a335e1b36a732b0ad75050e
kanogadhiya <89954301+kanogadhiya@users.noreply.github.com>
2021-09-17 09:37:29 +0530

    Add files via upload

--- j1.cpp
+++ j1.cpp
@@ -5,54 +5,56 @@ class electricity
 {
     char name[10];
     int units;
-    float charge=0;
+    float charge;
     public:
     electricity()
-    {
+    { 
         cout<<"Enter Your name :"<<endl;
-        gets(name);
-        cout<<"Enter Your Total :"<<endl;
+        cin>>name;
+        cout<<"Enter Your Total Units :"<<endl;
         cin>>units;
     }
     void calculate()
     {   if (units>0)
-        {   charge=50;
-            if (units>0 || units<=100)
+        {   
+            charge= 50.00;
+            if (units>0)
             {
                 if (units<100)
-                    (charge+=units*0.60); 
+                    cout<<(charge=charge+(units*0.60)); 
                 else 
-                    (charge+=100*0.60);
+                    charge+=100*0.60;
             }
-            if (units>100 || units<=300)
+            if (units>100)
             {
                 if (units<300)
-                    (charge+=(units-100)*0.80);
+                    charge+=(units-100)*0.80;
 
                 else 
 
-                    (charge+=200*0.80);
+                    charge+=200*0.80;
             }
 
             if (units>300)
-                (charge+=(units-300)*0.90);
+                charge+=(units-300)*0.90;
             if (charge>300)
-                charge+=charge*.15;
+                charge+=charge*0.15;
         }
         
         else
             cout<<"Please Enter Valid Units.!!";
-        display();
     }
     void display()
     {
         cout<<"Your name is :"<<name <<endl;
-        cout<<"Your Total amount of electricity bill is :"<<charge<<"Rs."<<endl;
+        cout<<"Your Total amount of electricity bill is :"<<charge<<" Rs."<<endl;
     }
 };
 int main()
 {
     electricity e1;
     e1.calculate();
+    e1.display();
+
     return 0;
 }
